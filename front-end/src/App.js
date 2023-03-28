0e4afb4dac3a29630eb89fc032ddad9a4811fca1
eagle9899 <eaglehe8@gmail.com>
2023-03-28 12:10:15 +0500

    First-commit

---
+++ front-end/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Home</a>
+      </header>
+      <main>List Products</main>
+    </div>
+  );
+}
